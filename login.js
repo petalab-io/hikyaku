bc7ccddb54b8e4577aaa5537e5ae74d8f3e7c1fc
petalab-io <petalab1024.5@gmail.com>
2025-12-12 00:02:06 +0900

    Feat: 環境変数から URL を読み込めるようにする#1
    環境変数対応を追加し、login.jsの対象URLを動的に設定可能に変更。また、npmコマンドを更新

--- login.js
+++ login.js
@@ -3,6 +3,9 @@ const {chromium} = require('playwright');
 const fs = require('fs');
 
 async function main() {
+    // 環境変数から URL を取得(Default値も設定可能)
+    const loginUrl = process.env.LOGIN_URL || 'https://sample.app';
+
     // 1. Profile なしの Clean な Browse を起動
     const browser = await chromium.launch({
         channel: 'chrome',
@@ -14,13 +17,14 @@ async function main() {
     const page = await context.newPage();
 
     console.log('--- ログイン情報 ---');
+    console.log(`対象URL: ${loginUrl}`);
     console.log('1. 開いたブラウザで対象サイトにログインしてください。');
     console.log('2. ログインが完了し、TOP ページ等が表示されたら、');
     console.log('   このターミナルに戻ってきて Enter キーを押してください。')
     console.log('-------------------');
 
-    // 対象 Site へ Access（必要に応じて URL を変えてください）
-    await page.goto('https://sample.app');
+    // 対象 Site へ Access
+    await page.goto(loginUrl);
 
     // 3. User の Enter キー入力を待つ
     await new Promise(resolve => {
